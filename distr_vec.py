22420126bce818564819df07ed0fd4a0b1169f76
Oliver Becher <oliver.becher@gmail.com>
2016-12-06 16:50:31 +0100

    push

--- distr_vec.py
+++ distr_vec.py
@@ -7,6 +7,10 @@ from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.cluster import DBSCAN
 from sklearn.preprocessing import StandardScaler
 
+"""
+This program calculates density distributions for all annotations and saves those as sparse vectors
+"""
+
 
 def get_ctg():
     all_id_to_ctg = {}
@@ -38,13 +42,6 @@ def filter_items(all_items, filter):
     return filtered_items
 
 
-def write_this(dist_vec):
-    with open('all_distributions.csv', 'wb') as out_file:
-        writer = csv.writer(out_file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        for line in dist_vec:
-            writer.writerow(line)
-
-
 def build_vectors(filtered_items, filters, length):
     all_d_vector = []
     i = 0
@@ -58,14 +55,13 @@ def build_vectors(filtered_items, filters, length):
         for item in filtered_items:
             if item[0] == filter:
                 for annot in item[1][2]:
-                    if annot in vector:
+                    if annot in vector.keys():
                         vector[annot] = vector[annot] + 1
                     else:
                         vector[annot] = 1
         for k,v in vector.iteritems():
             indeces.append([k,v])
         all_d_vector.append([filter, indeces])
-    write_this(all_d_vector)
     return all_d_vector
 
 
@@ -84,19 +80,14 @@ def main():
 
     filtered_items = filter_items(all_items, filters)
 
+    print len(filtered_items)
+
     all_distributions = build_vectors(filtered_items, filters, len(all_id_to_ctg))
 
-    """with open('all_distributions.csv', 'wb') as all_d:
-        writer = csv.writer(all_d, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        for d in all_distributions:
-            indexes = []
-            ind_cnt = 0
-            for ind in d[1]:
-                if ind > 0.0001:
-                    indexes.append([ind_cnt, ind])
-                ind_cnt += 1
-            writer.writerow([d[0], indexes])
-    """
+    with open('all_distributions.csv', 'wb') as out_file:
+        writer = csv.writer(out_file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for line in all_distributions:
+            writer.writerow(line)
 
 
 if __name__ == "__main__":
