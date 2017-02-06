5886efa7960904da6cdacae67e3d22bd40ea86b1
Oliver Becher <oliver.becher@gmail.com>
2017-02-06 22:53:05 +0100

    just do it

---
+++ 3_sleeping/DBSCAN.py
@@ -0,0 +1,140 @@
+import csv
+import sys, string
+import numpy as np
+import sklearn
+from sklearn.cluster import DBSCAN, Birch
+from sklearn import metrics
+from sklearn.preprocessing import StandardScaler
+from scipy.sparse import lil_matrix
+import matplotlib.pyplot as plt
+
+
+csv.field_size_limit(sys.maxsize)
+path = sys.argv[1]
+def load_distr():
+    all_d_content = []
+    allchars = ''.join(chr(i) for i in xrange(256))
+    identity = string.maketrans('', '')
+    nondigits = allchars.translate(identity, string.digits)
+    with open(path + 'all_distributions.csv') as distr_vec:
+        reader = csv.reader(distr_vec, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for row in reader:
+            features = [int(k.translate(identity, nondigits)) for k in row[1].split(",")]
+            l_features = [features[x] for x in range(0,len(features),2)]
+            xy_features = [(k,v) for k,v in {l_features[y/2-1]:features[y] for y in range(1,len(features),2)}.items()]
+            all_d_content.append([int(row[0]),xy_features])
+    return all_d_content
+
+
+def get_ctg():
+    all_id_to_ctg = {}
+    all_ctg_to_id = {}
+    with open(path + 'annotation_to_index.csv') as annotation_dict:
+        reader = csv.reader(annotation_dict, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for row in reader:
+            all_id_to_ctg[int(row[1])] = row[0]
+            all_ctg_to_id[row[0]] = int(row[1])
+    return all_id_to_ctg
+
+
+def build_sparse(all_d_content, lilx, lily):
+    positions = []
+    data = []
+    for distr in all_d_content:
+        this_position = []
+        this_data = []
+        for tuple in distr[1]:
+            this_position.append(tuple[0])
+            this_data.append(tuple[1])
+        positions.append(this_position)
+        data.append(this_data)
+    sparse_entities = lil_matrix((lilx, lily))
+    sparse_entities.rows = positions
+    sparse_entities.data = data
+    sparse_entities.tocsr()
+    return sparse_entities
+
+
+def main():
+    print "Loading data"
+    all_d_content = load_distr()
+    all_id_to_ctg = get_ctg()
+
+    print "Create sparse vectors"
+    sparse_entities = build_sparse(all_d_content, len(all_d_content), len(all_id_to_ctg))
+    del all_id_to_ctg, all_d_content
+
+    sparse_entities = StandardScaler(with_mean=False).fit_transform(sparse_entities)
+    #-------------------------------------------------------------------------------------
+
+    print "BIRCH"
+    brc = Birch(branching_factor=50, n_clusters=None, threshold=0.5, compute_labels=True).fit(sparse_entities)
+    labels = brc.labels_
+    centroids = brc.subcluster_centers_
+    n_clusters = np.unique(labels).size
+    print("n_clusters : %d" % n_clusters)
+
+    unique_labels = set(labels)
+    colors = plt.cm.Spectral(np.linspace(0, 1, len(unique_labels)))
+
+    birch_target_entities = []
+    for k, col in zip(unique_labels, colors):
+        if k == -1:
+            # Black used for noise.
+            col = 'k'
+
+        class_member_mask = (labels == k)
+        this_class = []
+        idx = -1
+        while True:
+            try:
+                idx = class_member_mask.tolist().index(True, idx+1)
+                this_class.append(idx)
+            except ValueError:
+                break
+        birch_target_entities.append(this_class)
+    print birch_target_entities
+
+    #-------------------------------------------------------------------------------------
+    print "Precomputing Distances"
+    XX = sklearn.metrics.pairwise.cosine_similarity(sparse_entities)
+    print "Clustering.."
+    db = DBSCAN(eps=0.3, min_samples=10, metric='precomputed').fit(XX)
+    core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+    core_samples_mask[db.core_sample_indices_] = True
+    labels = db.labels_
+    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+
+    print('Estimated number of clusters: %d' % n_clusters_)
+    print("Silhouette Coefficient: %0.3f"
+          % metrics.silhouette_score(all_d_content, labels))
+
+    unique_labels = set(labels)
+    colors = plt.cm.Spectral(np.linspace(0, 1, len(unique_labels)))
+
+    target_entities = []
+    for k, col in zip(unique_labels, colors):
+        if k == -1:
+            # Black used for noise.
+            col = 'k'
+
+        class_member_mask = (labels == k)
+        this_class = []
+        idx = -1
+        while True:
+            try:
+                idx = class_member_mask.tolist().index(True, idx+1)
+                this_class.append(idx)
+            except ValueError:
+                break
+        target_entities.append(this_class)
+
+    print target_entities
+    #with open(path + 'all_distr_weighted.csv', 'wb') as outfile:
+    #    writer = csv.writer(outfile, delimiter=',', quotechar='|',quoting=csv.QUOTE_MINIMAL)
+    #    for line in target_entities:
+    #        writer.writerow(line)
+
+
+if __name__ == "__main__":
+    main()
---
+++ time_analysis.py
@@ -0,0 +1,159 @@
+
+import csv
+import sys
+from scipy.stats import entropy
+from sklearn.metrics import pairwise as pw
+import string
+import warnings
+import time
+import datetime
+warnings.filterwarnings("ignore")
+
+"""
+The purpose of this program is to do an investigation of the data which was produced with the create vectors tool.
+You have to specify one or several IDs of annotations to investigate about (ID mapping can be found in separate csv file,
+produce by create vector tool as well)
+In detail, this tool creates density distribution vectors for the specified annotations and compares them to each other
+ with a KL divergence / cosine metric to measure similarity.
+ All output will be printed on screen.
+"""
+path = sys.argv[1]
+csv.field_size_limit(sys.maxsize)
+
+q_char = '|'
+
+
+def timeframes(concept, window):
+    start_date = time.strptime("2014-08-13", "%Y-%m-%d")
+    end_date = time.strptime("2015-08-13", "%Y-%m-%d")
+    all_frames = []
+    this_date = start_date
+    while this_date <= end_date:
+        this_date = this_date + datetime.timedelta(days=window)
+        all_frames.append(this_date)
+    return timeframes()
+
+
+def get_ctg(filters):
+    filter_id_to_ctg = {}
+    all_id_to_ctg = {}
+    with open(path + 'annotation_to_index.csv') as annotation_dict:
+        reader = csv.reader(annotation_dict, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for row in reader:
+            if int(row[1]) in filters:
+                filter_id_to_ctg[int(row[1])] = row[0]
+            all_id_to_ctg[int(row[1])] = row[0]
+    return filter_id_to_ctg, all_id_to_ctg
+
+
+def load_distr(filters):
+all_d_content = []
+allchars = ''.join(chr(i) for i in xrange(256))
+identity = string.maketrans('', '')
+nondigits = allchars.translate(identity, string.digits)
+with open(path + 'all_distributions_time.csv') as distr_vec:
+    reader = csv.reader(distr_vec, delimiter=',', quotechar=q_char, quoting=csv.QUOTE_MINIMAL)
+    all_data = []
+        for row in reader:
+            all_data.append(row)
+        for filter in filters:
+            row = all_data[filter]
+            features = [int(k.translate(identity, nondigits)) for k in row[1].split(",")]
+            l_features = [features[x] for x in range(0,len(features),2)]
+            xy_features = [(k,v) for k,v in {l_features[y/2-1]:features[y] for y in range(1,len(features),2)}.items()]
+            all_d_content.append([int(row[0]),xy_features])
+        del all_data
+    return all_d_content
+
+
+def rebuild_distr(all_d_content, vlen):
+    #print all_d_content
+    vlen = 111953
+    all_d_vec = []
+    for d_vec in all_d_content:
+        d_vector = [0.000001] * vlen
+        d_id = d_vec[0]
+        for keyval in d_vec[1]:
+            d_vector[keyval[0]] = keyval[1]
+        #print "     " + str(sum(d_vector))
+        all_d_vec.append([d_id, d_vector])
+    return all_d_vec
+
+
+# might be problem: distributions might not contain a specific vector --> need identifier
+def kl_div(distr):
+    all_div = []
+    for i in range(0,len(distr)-1):
+        this_distr = distr[i][1]
+        next_distr = distr[i+1][1]
+        if sum(this_distr) < 1 or sum(next_distr) < 1:
+            print "Cant compare quarter " + str(i+1) + " to " + str(i+2) + ". One Vector empty: " + str(sum(this_distr)) + " or " + str(sum(next_distr))
+            all_div.append('NaN')
+            continue
+        this_entropy = entropy(this_distr, next_distr)
+        all_div.append(this_entropy)
+    all_div.append(entropy(distr[0][1], distr[len(distr)-1][1]))
+    return all_div
+
+
+def validation_scoring(distributions):
+    scoring = [0] * len(distributions)
+    gold_standard = distributions[0]
+    k = 0
+    for d in distributions:
+        for i in range(0,len(d[1])):
+            if (d[1][i] >= 0.00001) and (gold_standard[1][i] >= 0.00001):
+                scoring[k] += 1
+        k += 1
+    return scoring
+
+
+def remap(distributions, filters):
+    distr_per_id = []
+    for i in range(0,len(filters)):
+        this_dist = []
+        this_dist.append(distributions[0][i])
+        this_dist.append(distributions[1][i])
+        this_dist.append(distributions[2][i])
+        this_dist.append(distributions[3][i])
+        distr_per_id.append(this_dist)
+    return distr_per_id
+
+
+
+def main(argv):
+    filters = map(int, argv[2:])
+    print "Setting filters.. "
+
+    filter_id_to_ctg, all_id_to_ctg = get_ctg(filters)
+
+    for filter in filters:
+        print "     " + str(filter) + ": " + str(filter_id_to_ctg[filter].split('/')[-1])
+
+    distributions = load_distr(filters)
+
+    print "Create sparse vectors"
+    for i in range(0,len(distributions)):
+        distributions[i] = rebuild_distr(distributions[i], len(all_id_to_ctg))
+
+    print "Built " + str(len(distributions[0])) + " probability density vectors"
+
+    distributions = remap(distributions, filters)
+
+    divergences_per_id = []
+    for distr in distributions:
+        divergences_per_id.append(kl_div(distr))
+    for i in range(0,len(filters)):
+        print "KL Divergences within filter " + str(filter_id_to_ctg[filters[i]].split('/')[-1]) + " are: "
+
+        print "     Quarter 1 to Quarter 2: " + str(divergences_per_id[i][0])
+        print "     Quarter 2 to Quarter 3: " + str(divergences_per_id[i][1])
+        print "     Quarter 3 to Quarter 4: " + str(divergences_per_id[i][2])
+        print "     Quarter 1 to Quarter 4: " + str(divergences_per_id[i][3])
+        print ""
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print "Usage: enter one or several indices from the dictionary hinting at wikipedia annotations which are preferably related. This program calculates the cosine similarity between the first entered index and all other entered indeces."
+    else:
+        main(sys.argv)
