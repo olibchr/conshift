5c7d01a53c1f50d293f6b005baa4bf5381d2872a
Oliver Becher <oliver.becher@gmail.com>
2017-06-13 14:12:22 +0200

    cleanup

--- evaluation_manager.py
+++ evaluation_manager.py
@@ -5,9 +5,11 @@ import multiprocessing
 
 
 path = '/Users/oliverbecher/1_data/0_cwi/1_data/'
+#path = '/export/scratch1/home/becher/data/'
 conceptsPerRequest = 6
 
-concepts = int(sys.argv[1])
+num_concepts = int(sys.argv[1])
+
 
 def get_ind_cnt():
     all_ind = []
@@ -30,22 +32,16 @@ def get_ranges(ind_cnt):
 
 
 def get_indeces(offset1, offset2):
-    if concepts/3 > offset1: top_cnt = random.sample(xrange(concepts/3), concepts/3)
+    if num_concepts/3 > offset1: top_cnt = random.sample(xrange(num_concepts / 3), num_concepts / 3)
     else:
-        top_cnt = random.sample(xrange(offset1), concepts/3)
-    if concepts/3 > offset2: med_cnt = random.sample(xrange(concepts/3),concepts/3)
+        top_cnt = random.sample(xrange(offset1), num_concepts / 3)
+    if num_concepts/3 > offset2: med_cnt = random.sample(xrange(num_concepts / 3), num_concepts / 3)
     else:
-        med_cnt = random.sample(xrange(offset1, offset2), concepts/3)
-    low_cnt = random.sample(xrange(offset1+offset2, 71564), concepts/3)
+        med_cnt = random.sample(xrange(offset1, offset2), num_concepts / 3)
+    low_cnt = random.sample(xrange(offset1+offset2, 71564), num_concepts / 3)
     all_indeces = top_cnt + med_cnt + low_cnt
     return all_indeces
 
-
-def start_processes(procs):
-    for proc in procs:
-        proc.start()
-    for proc in procs:
-        proc.join()
 
 all_ind_cnt = get_ind_cnt()
 offset1, offset2 = get_ranges(all_ind_cnt)
@@ -58,19 +54,3 @@ print filter_badges
 num_cores = multiprocessing.cpu_count()
 Parallel(n_jobs=2)(delayed(wiki_comparison.experiment_1)(badge) for badge in filter_badges)
 
-
-"""
-procs = []
-filter_offset = 0
-while filter_offset < len(filters):
-    print filter_offset,len(filters)
-    if filter_offset+conceptsPerRequest >= len(filter_indeces): end = len(filter_indeces)
-    else: end = filter_offset + conceptsPerRequest
-    this_filters = filters[filter_offset:end]
-    filter_offset += conceptsPerRequest
-    proc = Process(target=wiki_comparison.experiment_1(this_filters))
-    procs.append(proc)
-    if len(procs) >= 2 or filter_offset >= len(filters):
-        start_processes(procs)
-    #wiki_comparison.experiment_1(this_filters)
-"""
