fd0dc005889d0fecaf2e6c86da0f1cb28a1370ca
Oliver Becher <oliver.becher@gmail.com>
2017-06-20 17:41:51 +0200

    fix

--- run_exps.sh
+++ run_exps.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python evalutation_manager 1 6 | tee output.txt &
+python evaluation_manager.py 1 6 | tee output.txt &
 wait
-#python evalutation_manager 2 990 | tee output.txt &
+#python evaluation_manager.py 2 990 | tee output.txt &
