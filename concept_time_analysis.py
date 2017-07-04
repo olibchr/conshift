03b08c178502d9bbe235c9398b193f74f5d4dd62
Oliver Becher <oliver.becher@gmail.com>
2017-07-04 12:05:11 +0200

    prints

--- concept_time_analysis.py
+++ concept_time_analysis.py
@@ -55,15 +55,9 @@ class Concept():
         tag_quad = [[int(item.split('_')[0]), int(item.split('_')[1]), datetime.datetime.strptime(docid_to_date[int(item.split('_')[1])], "%Y-%m-%d"), 1] for item in self.features]
         tag_quad = sorted(tag_quad, key=lambda date: (date[2], date[1]))
         tag_len = len(set([tag[1] for tag in tag_quad]))
-        #print('PREDEF BUCKETSIZE: {}'.format(bucketsize))
         if tag_len <= 2 * bucketsize: buckets=2
-        else: buckets = int(math.ceil(tag_len/(1.0 * bucketsize))); print tag_len/(1.0 * bucketsize)
-        #print('TAGLEN: {}'.format(tag_len))
-        #print('TMP BUCKET: {}'.format(buckets))
-        #bucketsize = #int(round(tag_len / (1.0 * buckets)))
-        #print('TMP BUCKETSIZE: {}'.format(bucketsize))
+        else: buckets = int(math.ceil(tag_len/(1.0 * bucketsize)))
         all_buckets = [dict() for x in range(buckets)]
-        #print len(all_buckets)
         all_last_adds = buckets * [datetime.datetime.strptime("2015-08-14", "%Y-%m-%d")]
         i = 0
         t = 0
