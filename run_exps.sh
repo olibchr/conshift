0464759aeb8e3334f07d89b2736d75004f2f090d
Oliver Becher <oliver.becher@gmail.com>
2017-07-04 12:09:51 +0200

    exp

--- run_exps.sh
+++ run_exps.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-python evaluation_manager.py 1 990 | tee output.txt &
+python experiment_manager.py 1 990 | tee output.txt &
 wait
-python evaluation_manager.py 2 990 | tee output2.txt &
+python experiment_manager.py 2 990 | tee output2.txt &
 wait
-python evaluation_manager.py 3 990 | tee output3.txt &
+python experiment_manager.py 3 990 | tee output3.txt &
 wait
-python evaluation_manager.py 4 990 | tee output4.txt &
+python experiment_manager.py 4 990 | tee output4.txt &
 wait
